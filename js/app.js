0ef0cb23e0ab03c4293cc1d5b01be9905d91f95d
Jonathan Dela Pena <jonathancastillondelapena28gmail.com>
2023-01-17 14:46:05 +0800

    Trying to implement Navbar sticky top using jquery

---
+++ js/app.js
@@ -0,0 +1,13 @@
+(function ($) {
+    `use strict`;
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('.navbar').addClass('sticky-top');
+        } else {
+            $('.navbar').removeClass('sticky-top');
+        }
+    });
+
+})(jquery);
